cb3945bd27626de0198b25a8c2a32d04898cf73a
sarachen19 <49016705+sarachen19@users.noreply.github.com>
2020-10-07 14:52:07 -0400

    Create Item.cs

---
+++ AspNetTeam_Prj/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetTeam_Prj.Models
+{
+    public class Item
+    {
+        public int Item_id { get; set; }
+        public int User_id { get; set; }
+        public string Name { get; set; }
+        public decimal Listing_price { get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }
+        public int Like_count { get; set; }
+    }
+}
