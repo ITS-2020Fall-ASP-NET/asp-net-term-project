fea8a54be44825f4058a314eeb0146e92e8d3ed0
Hansol Jo <hansol.jo@gmail.com>
2020-10-06 23:02:55 -0400

    Boilerplate codes for the first submission.

---
+++ AspNetTeam_Prj/Controllers/ItemDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetTeam_Prj.Controllers
+{
+    public class ItemDetailController : Controller
+    {
+        // GET: ItemDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
